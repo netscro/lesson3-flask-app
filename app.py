8ae7873f85b038af303ed452441752660b092578
netscro <kovalev_evgeniy@list.ru>
2020-11-19 23:58:53 +0200

    working programm

--- app.py
+++ app.py
@@ -8,17 +8,17 @@ f = Fernet(key)
 
 @app.route('/encrypt')
 def encrypt():
-    param = request.args.get('param', '')
-    string = f.encrypt(param.encode())
-    return render_template('index.html', string=string)
+    string = request.args.get('string', '')
+    param = f.encrypt(string.encode()).decode()
+    return render_template('index.html', string=param)
 
 
 @app.route('/decrypt')
 def decrypt():
-    param = request.args.get('param', '')
-    print(param.encode())
-    string = f.decrypt(param.encode())
-    return render_template('index.html', string=string)
+    string = request.args.get('string', '')
+    print(string.encode())
+    param = f.decrypt(string.encode()).decode()
+    return render_template('index.html', string=param)
 
 
 app.run(debug=True)
