d58f3dea263e868be351c946735c5db461d878cd
netscro <kovalev_evgeniy@list.ru>
2020-11-19 16:40:08 +0200

    work on one page

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from cryptography.fernet import Fernet
+
+app = Flask(__name__)
+key = Fernet.generate_key()
+f = Fernet(key)
+
+
+@app.route('/encrypt')
+def encrypt():
+    string = request.args.get('string')
+    token = f.encrypt(string.encode())
+    original_token = token.decode()
+    d = f.decrypt(token).decode()
+    return render_template('index.html', original_token=original_token, string=d)
+
+
+app.run(debug=True)
